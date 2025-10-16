34dada9344a22aaaaf96b5f4420c4cd909f7e7e3
Quách Gia Bảo <baoqg9104@gmail.com>
2025-10-16 15:44:20 +0700

    style: format email service

--- Labverse.BLL/Services/EmailService.cs
+++ Labverse.BLL/Services/EmailService.cs
@@ -3,34 +3,33 @@ using Microsoft.Extensions.Configuration;
 using System.Net;
 using System.Net.Mail;
 
-namespace Labverse.BLL.Services
+namespace Labverse.BLL.Services;
+
+public class EmailService : IEmailService
 {
-    public class EmailService : IEmailService
+    private readonly IConfiguration _configuration;
+
+    public EmailService(IConfiguration configuration)
     {
-        private readonly IConfiguration _configuration;
+        _configuration = configuration;
+    }
 
-        public EmailService(IConfiguration configuration)
+    public async Task SendEmailAsync(string to, string subject, string body)
+    {
+        var smtpHost = _configuration["Smtp:Host"];
+        var smtpPort = int.Parse(_configuration["Smtp:Port"]);
+        var smtpUser = _configuration["Smtp:Username"];
+        var smtpPass = _configuration["Smtp:Password"];
+        var from = _configuration["Smtp:From"];
+
+        using var client = new SmtpClient(smtpHost, smtpPort)
         {
-            _configuration = configuration;
-        }
+            Credentials = new NetworkCredential(smtpUser, smtpPass),
+            EnableSsl = true,
+        };
 
-        public async Task SendEmailAsync(string to, string subject, string body)
-        {
-            var smtpHost = _configuration["Smtp:Host"];
-            var smtpPort = int.Parse(_configuration["Smtp:Port"]);
-            var smtpUser = _configuration["Smtp:Username"];
-            var smtpPass = _configuration["Smtp:Password"];
-            var from = _configuration["Smtp:From"];
+        var mailMessage = new MailMessage(from, to, subject, body) { IsBodyHtml = true };
 
-            using var client = new SmtpClient(smtpHost, smtpPort)
-            {
-                Credentials = new NetworkCredential(smtpUser, smtpPass),
-                EnableSsl = true,
-            };
-
-            var mailMessage = new MailMessage(from, to, subject, body) { IsBodyHtml = true };
-
-            await client.SendMailAsync(mailMessage);
-        }
+        await client.SendMailAsync(mailMessage);
     }
 }
