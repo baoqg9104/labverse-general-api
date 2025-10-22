276b5db9084d96341295f7ccf0e230ba421aee1b
Quách Gia Bảo <baoqg9104@gmail.com>
2025-10-23 00:02:33 +0700

    fix: update user

--- Labverse.BLL/DTOs/Users/CreateUserDto.cs
+++ Labverse.BLL/DTOs/Users/CreateUserDto.cs
@@ -22,10 +22,10 @@ public class CreateUserDto
         MinimumLength = 4,
         ErrorMessage = "Username must be between 4 and 100 characters"
     )]
-    [RegularExpression(
-        "^[A-Za-z][A-Za-z0-9_]{3,99}$",
-        ErrorMessage = "Username must start with a letter and contain only letters, numbers, and underscores"
-    )]
+    //[RegularExpression(
+    //    "^[A-Za-z][A-Za-z0-9_ ]{3,99}$",
+    //    ErrorMessage = "Username must start with a letter and contain only letters, numbers, spaces, and underscores"
+    //)]
     public string Username { get; set; }
 
     public string RecaptchaToken { get; set; }
